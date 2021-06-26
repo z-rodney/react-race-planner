3bf4916425871de9b6db8a348f8a088486efecda
Zaina Rodney <zainarodney@gmail.com>
2021-06-26 00:03:50 -0400

    added custom useInput hook

--- src/components/AddStageRaceForm.tsx
+++ src/components/AddStageRaceForm.tsx
@@ -1,16 +1,30 @@
-import React from "react";
-import { Container, SecondaryOutlineButton, SuccessOutlineButton, DangerOutlineButton } from "./shared";
+import * as React from "react";
+import { useState } from "react";
+import { Container, SecondaryOutlineButton, SuccessOutlineButton, DangerOutlineButton, FormInputGroup } from "./shared";
 
 type AddStageFormProps = {
   toggleOpen: () => void
 }
 
-export const AddStageRaceForm: React.FC<AddStageFormProps> = ({toggleOpen}) => {
+const useInput: (val: string) => [string, (ev: React.ChangeEvent<HTMLInputElement>) => void] = (val) => {
+  const [name, setName] = useState<string>(val);
 
+  const handleChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    setName(ev.target.value)
+  }
+
+  return [name, handleChange]
+}
+
+export const AddStageRaceForm: React.FC<AddStageFormProps> = ({toggleOpen}) => {
+  const [name, setName] = useInput('');
 
   return (
     <Container>
       <h2>Add Stage Race</h2>
+      <FormInputGroup id="stageRaceName" placeholder="Enter stage race name" name="name" value={name} onChange={setName}>
+
+      </FormInputGroup>
       <h2>Stages</h2>
       <SecondaryOutlineButton>
         Add Stage
